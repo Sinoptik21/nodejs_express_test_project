85b9df210038de8bd1bdac20e12ca291acbbbf5e
Sinoptik21 <anton@bokus.ru>
2016-10-28 13:43:52 +0300

    gulpfile.js fix

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,21 @@
 'use strict';
 
 const gulp = require('gulp'),
-      mocha = require('gulp-mocha'),
+      gmocha = require('gulp-mocha'),
       jshint = require('gulp-jshint'),
       exec = require('child_process').exec;
 
-gulp.task('mocha', () => 
-  gulp.src('qa/tests-*.js', {read: false})
-    .pipe(mocha({ui: 'tdd'}))
-);
+gulp.task('mocha', () => {
+  let stream = gulp.src('qa/tests-*.js', {read: false})
+    .pipe(gmocha({ui: 'tdd'}));
+  return stream;
+});
 
 gulp.task('jshint', () => {
-  gulp.src(['meadowlark.js', 'public/js/**/*.js', 'lib/**/*.js', 'gulpgile.js', 'qa/**/*.js'])
+  let stream = gulp.src(['meadowlark.js', 'public/js/**/*.js', 'lib/**/*.js', 'gulpfile.js', 'qa/**/*.js'])
     .pipe(jshint())
     .pipe(jshint.reporter('default'));
+  return stream;
 });
 
 gulp.task('linkchecker', (cb) => {
